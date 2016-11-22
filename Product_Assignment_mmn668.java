cbb6afa48b12da23c0e381064738a3c7a3a43226
mnix13 <mnix@utexas.edu>
2016-11-22 14:58:42 -0600

    completed assignment locally

---
+++ Product_Assignment_mmn668.java
@@ -0,0 +1,8 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_Assignment_<<eid>>.java   (Product_Assignment_jr007.java)
+// It should have a private sku instance variable containing your eid.
+
+class Product_Assignment_mmn668 {
+	private String eid = "mmn668";
+}
